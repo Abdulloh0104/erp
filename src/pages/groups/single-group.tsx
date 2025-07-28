90d9d6e352040d4051d104ab8826b444b0fa2caf
Abdulloh0104 <aar010407@gmail.com>
2025-07-28 10:37:35 +0500

    phone

--- src/pages/groups/single-group.tsx
+++ src/pages/groups/single-group.tsx
@@ -5,7 +5,7 @@ import { GroupLessons } from "@components";
 
 const SingleGroup = () => {
   const { id } = useParams<{ id: string }>();
-  // const { students, lessons, teachers } = useGroup(
+  // const {students,lessons,teachers } = useGroup(
   const {lessons} = useGroup(
     { page: 1, limit: 10 },
     Number(id)
